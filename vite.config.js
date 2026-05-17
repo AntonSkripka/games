fcb9b4d37075374f7358591582409538240415ef
Antooon32 <antonskripka11@gmail.com>
2026-05-17 21:10:23 +0300

    fix part-1

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import FullReload from 'vite-plugin-full-reload'
 export default defineConfig({
   base: '/games/',
   define: {
-    global: {},
+    global: 'globalThis',
   },
   root: 'src',
   build: {
